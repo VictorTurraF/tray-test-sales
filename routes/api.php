19e8499a651af576a16d16b82a550d4b220787ae
Victor Turra <victor.florencio@univem.edu.br>
2023-09-21 21:36:02 -0300

    refactor: remove unused features

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/sellers', function (Request $request) {
 
     $sellerName = $request->input("name");
