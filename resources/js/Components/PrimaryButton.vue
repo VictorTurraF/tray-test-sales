4fc08b52238f36bfaf4aeec11190c27b44e0e4a3
Victor Turra <victor.florencio@univem.edu.br>
2023-09-21 23:29:53 -0300

    style: seller list stylized

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,7 +1,23 @@
 <template>
-    <button
-        class="inline-flex items-center px-5 py-3 bg-gray-800 border border-transparent rounded-md font-semibold text-sm text-white uppercase hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-    >
-        <slot />
-    </button>
+  <button
+    :class="[
+      'inline-flex items-center ',
+      small ? 'px-3 py-2' : 'px-5 py-3',
+      small ? 'text-xs' : 'text-sm',
+      'bg-gray-800 border border-transparent rounded-md font-semibold uppercase',
+      'text-white hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900',
+      'focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+      'transition ease-in-out duration-150'
+    ]"
+  >
+    <slot />
+  </button>
 </template>
+
+<script>
+export default {
+  props: {
+    small: Boolean, // Add a prop for the small variant
+  },
+};
+</script>
--- resources/js/Pages/SellerList.vue
+++ resources/js/Pages/SellerList.vue
@@ -1,40 +1,52 @@
 <template>
-  <div>
-    <h1>Lista de Vendedores</h1>
-    <button @click="visit('/vendedores/cadastrar')">Adicionar vendedor</button>
-    <button @click="visit('/')">Voltar</button>
-    <ul>
-      <li v-for="seller in sellers" :key="seller.id">
+  <Box style="width: 700px;">
+    <Title class="mb-4">Lista de Vendedores</Title>
+    <div class="flex flex-row gap-4">
+      <SecondaryButton @click="visit('/')">Voltar</SecondaryButton>
+      <PrimaryButton @click="visit('/vendedores/cadastrar')">Adicionar vendedor</PrimaryButton>
+    </div>
+    <div class="mt-4 rounded-md bg-gray-900 text-white font-bold px-3 py-2 grid grid-cols-3 gap-4 items-center mb-3" style="grid-template-columns: 1fr 1fr 148px;">
+      <span>Nome</span>
+      <span>Email</span>
+      <span>Ações</span>
+    </div>
+    <ul class="flex flex-col gap-3 rounded-md">
+      <li class="rounded-md bg-gray-300 px-3 py-2 grid grid-cols-3 gap-4 items-center" style="grid-template-columns: 1fr 1fr auto;" v-for="seller in sellers" :key="seller.id">
         <span>{{ seller.name }}</span>
         <span>{{ seller.email }}</span>
-        <button @click="viewSellerOrders(seller.id)">Ver vendas</button>
+        <PrimaryButton :small="true" @click="viewSellerOrders(seller.id)">Visualizar vendas</PrimaryButton>
       </li>
     </ul>
-  </div>
+  </Box>
 </template>
 
 <script>
+import Box from "@/Components/Box.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import Title from "@/Components/Title.vue";
 import axios from "axios";
 
 export default {
-  methods: {
-    visit(route) {
-      this.$inertia.visit(route);
+    methods: {
+        visit(route) {
+            this.$inertia.visit(route);
+        },
+        viewSellerOrders(sellerId) {
+            this.$inertia.visit(`/vendedores/${sellerId}/vendas`);
+        },
     },
-    viewSellerOrders(sellerId) {
-      this.$inertia.visit(`/vendedores/${sellerId}/vendas`);
+    data() {
+        return {
+            sellers: [],
+        };
     },
-  },
-  data() {
-    return {
-      sellers: [],
-    };
-  },
-  mounted() {
-    // Fetch the list of sellers from the API using Axios
-    axios.get("/api/sellers").then((response) => {
-      this.sellers = response.data.data;
-    });
-  },
+    mounted() {
+        // Fetch the list of sellers from the API using Axios
+        axios.get("/api/sellers").then((response) => {
+            this.sellers = response.data.data;
+        });
+    },
+    components: { Box, Title, SecondaryButton, PrimaryButton }
 };
 </script>
