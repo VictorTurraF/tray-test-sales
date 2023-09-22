16ffd1d075ed0673854242887a917e5bf40d582b
Victor Turra <victor.florencio@univem.edu.br>
2023-09-21 23:03:01 -0300

    style: add seller page stylized

---
+++ resources/js/Components/Box.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="block p-7 bg-gray-100 mx-auto rounded-lg mt-12" style="width: 500px;">
+    <slot />
+  </div>
+</template>
--- resources/js/Pages/AddSeller.vue
+++ resources/js/Pages/AddSeller.vue
@@ -1,48 +1,80 @@
 <template>
-  <div>
-    <h1>Create Seller</h1>
-    <button @click="visit('/vendas')">Voltar</button>
-    <form @submit.prevent="createSeller">
-      <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="seller.name" required />
+  <Box>
+    <Title class="mb-4">Adicionar Novo Vendedor</Title>
+    <form class="flex flex-col gap-3 items-start" @submit.prevent="createSeller">
+      <div class="flex flex-col gap-2">
+        <InputLabel for="name">Name:</InputLabel>
+        <TextInput
+          placeholder="Nome do vendedor"
+          type="text"
+          id="name"
+          v-model="seller.name"
+          required
+        />
       </div>
-      <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="seller.email" required />
+      <div class="flex flex-col gap-2">
+        <InputLabel for="email">Email:</InputLabel>
+        <TextInput
+          placeholder="Email do vendedor"
+          type="email"
+          id="email"
+          v-model="seller.email"
+          required
+        />
       </div>
-      <button type="submit">Create Seller</button>
+      <div class="flex flex-row gap-4 mt-4">
+        <SecondaryButton @click="visit('/vendedores')">Voltar</SecondaryButton>
+        <PrimaryButton>Criar Vendedor</PrimaryButton>
+      </div>
     </form>
-  </div>
+  </Box>
 </template>
 
+
+
 <script>
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Title from '@/Components/Title.vue';
+import Box from '@/Components/Box.vue';
+import InputError from '@/Components/InputError.vue';
+
 import axios from "axios";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 export default {
-  data() {
-    return {
-      seller: {
-        name: "",
-        email: "",
-      },
-    };
-  },
-  methods: {
-    visit(route) {
-      this.$inertia.visit(route);
+    data() {
+        return {
+            seller: {
+                name: "",
+                email: "",
+            },
+        };
     },
-    createSeller() {
-      // Send a POST request to create a seller
-      axios.post("/api/sellers", this.seller).then((response) => {
-        // Handle the response as needed (e.g., show a success message)
-        console.log("Seller created:", response.data.data);
-
-        // Clear the form fields
-        this.seller.name = "";
-        this.seller.email = "";
-      });
+    methods: {
+        visit(route) {
+            this.$inertia.visit(route);
+        },
+        createSeller() {
+            // Send a POST request to create a seller
+            axios.post("/api/sellers", this.seller).then((response) => {
+                // Handle the response as needed (e.g., show a success message)
+                console.log("Seller created:", response.data.data);
+                // Clear the form fields
+                this.seller.name = "";
+                this.seller.email = "";
+            });
+        },
     },
-  },
+    components: {
+      TextInput,
+      InputLabel,
+      PrimaryButton,
+      Title,
+      SecondaryButton,
+      Box,
+      InputError
+    }
 };
 </script>
