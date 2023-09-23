cf63962f22911a1c97e13e5a154f6f9b84b5705f
Victor Turra <victor.florencio@univem.edu.br>
2023-09-22 22:16:34 -0300

    fix: timezone for now based generated dates

--- app/Services/SellerReportService.php
+++ app/Services/SellerReportService.php
@@ -11,7 +11,7 @@ class SellerReportService
     public function generateDailySalesReport(Seller $seller, $date)
     {
         // Calculate total sales and commissions for the seller
-        $totalSales = Order::where('seller_id', $seller->id)->where('payment_approved_at', $date)->sum('price_in_cents');;
+        $totalSales = Order::where('seller_id', $seller->id)->where('payment_approved_at', $date)->sum('price_in_cents');
         $totalCommissions = $totalSales * (8.5 / 100);
 
         return new SellerReportDTO(
